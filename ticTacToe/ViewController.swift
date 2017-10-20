71cc982cbba1d82eaa71a96c8d60a51d981f8404
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2017-10-20 12:49:25 -0400

    Origin Commit

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -9,9 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var allButtons: [GameButton] = []
+    @IBOutlet weak var ticTacToeLabel: UILabel!
+    @IBOutlet weak var playerTurnLabel: UILabel!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var loseLabel: UILabel!
+    @IBOutlet weak var playAgain: UIButton!
+    @IBOutlet weak var p1WinCount: UILabel!
+    @IBOutlet weak var p2WinCount: UILabel!
+    @IBOutlet weak var p1p2WinLabel: UILabel!
+    @IBOutlet weak var button11: GameButton!
+    @IBOutlet weak var button12: GameButton!
+    @IBOutlet weak var button13: GameButton!
+    @IBOutlet weak var button21: GameButton!
+    @IBOutlet weak var button22: GameButton!
+    @IBOutlet weak var button23: GameButton!
+    @IBOutlet weak var button31: GameButton!
+    @IBOutlet weak var button32: GameButton!
+    @IBOutlet weak var button33: GameButton!
+    
+    let ticTacToe = ticTacToeBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Execute code here
+        winLabel.isHidden = true
+        loseLabel.isHidden = true
+        p1WinCount.isHidden = true
+        p2WinCount.isHidden = true
+        p1p2WinLabel.isHidden = true
     }
+    
+    @IBAction func gameButtons(_ sender: GameButton) {
+        ticTacToe.clickABox()
+        if ticTacToe.playerTracker == true {
+            sender.setImage(#imageLiteral(resourceName: "xbutton"), for: .normal)
+            playerTurnLabel.text = "Player Two's Turn"
+        } else {
+            sender.setImage(#imageLiteral(resourceName: "oButton"), for: .normal)
+            playerTurnLabel.text = "Player One's Turn"
+        }
+        sender.isEnabled = false
+        func addToArray() {
+            if ticTacToe.playerTracker == true {
+                ticTacToe.playerOneArray.append((sender.column, sender.row))
+                ticTacToe.tagPlayerOneArray.append(sender.tag)
+            } else {
+                ticTacToe.playerTwoArray.append((sender.column, sender.row))
+                ticTacToe.tagPlayerTwoArray.append(sender.tag)
+            }
+        }
+        addToArray()
+        ticTacToe.checkWinner()
+        if ticTacToe.youWin == true {
+            if ticTacToe.playerTracker == true {
+                winLabel.text = "P1 Wins!"
+                winLabel.isHidden = false
+            } else {
+                winLabel.text = "P2 Wins!"
+                winLabel.isHidden = false
+            }
+            allButtons = [button11, button12, button13, button21, button22, button23, button31, button32, button33]
+            for button in allButtons {
+                button.isEnabled = false
+            }
+        }
+    }
+    
+    @IBAction func playAgainButton(_ sender: UIButton) {
+        allButtons = [button11, button12, button13, button21, button22, button23, button31, button32, button33]
+        ticTacToe.youWin = false
+        ticTacToe.playerOneArray = []
+        ticTacToe.playerTwoArray = []
+        for button in allButtons {
+            button.isEnabled = true
+            button.setImage(nil, for: .normal)
+        }
+        winLabel.isHidden = true
+        loseLabel.isHidden = true
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
--- ticTacToe/ticTacToeBrain.swift
+++ ticTacToe/ticTacToeBrain.swift
@@ -7,7 +7,93 @@
 //
 
 import Foundation
+import UIKit
+//THIS IS THE MODEL
 
 class ticTacToeBrain {
-
+    var playerTracker: Bool = false
+    var youWin = false
+    
+    //Tuple version of Array
+    var playerOneArray: [(Int, Int)] = []
+    var playerTwoArray: [(Int, Int)] = []
+    
+    //Tag version of Array
+    var tagPlayerOneArray: [Int] = []
+    var tagPlayerTwoArray: [Int] = []
+    
+    //Tuple version of win condition
+    let winLeftToRightDiagonal = [(1,1), (2,2), (3,3)]
+    let winRightToLeftDiagonal = [(1,3), (2,2), (3,1)]
+    let winTopRow = [(1,1), (1,2), (1,3)]
+    let winMiddleRow = [(2,1), (2,2), (2,3)]
+    let winBottomRow = [(3,1), (3,2), (3,3)]
+    let winRightColumn = [(1,3), (2,3), (3,3)]
+    let winMiddleColumn = [(1,2), (2,2), (3,2)]
+    let winLeftColumn = [(1,1), (2,1), (3,1)]
+    
+    //Tag version of win condition
+    let tagWinLeftToRightDiagonal = [1,5,9]
+    let tagWinRightToLeftDiagonal = [3,5,7]
+    let tagWinTopRow = [1,2,3]
+    let tagWinMiddleRow = [4,5,6]
+    let tagWinBottomRow = [7,8,9]
+    let tagWinRightColumn = [3,6,9]
+    let tagWinMiddleColumn = [2,5,8]
+    let tagWinLeftColumn = [1,4,7]
+    
+    
+    
+    func checkWinner () {
+        //for 
+        
+        
+        
+        
+        for (row, column) in playerOneArray where playerOneArray.count > 2 {
+            var containsDoesntWork = 0
+            for tuples in winTopRow {
+                if (row == tuples.0 && column == tuples.1) {
+                    containsDoesntWork += 1
+                    if (row == tuples.0 && column == tuples.1) {
+                        containsDoesntWork += 1
+                        if (row == tuples.0 && column == tuples.1) {
+                            containsDoesntWork += 1
+                            if containsDoesntWork == 3 {
+                                youWin = true
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (row, column) in playerTwoArray where playerTwoArray.count > 2 {
+            var containsDoesntWork = 0
+            for tuples in winTopRow {
+                if (row == tuples.0 && column == tuples.1) {
+                    containsDoesntWork += 1
+                    if (row == tuples.0 && column == tuples.1) {
+                        containsDoesntWork += 1
+                        if (row == tuples.0 && column == tuples.1) {
+                            containsDoesntWork += 1
+                            if containsDoesntWork == 3 {
+                                youWin = true
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func clickABox() {
+        if playerTracker == true {
+            playerTracker = false
+        } else {
+            playerTracker = true
+        }
+    }
 }

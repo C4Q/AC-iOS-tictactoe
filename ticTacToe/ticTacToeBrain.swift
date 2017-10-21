568f01150bda279996c27863f2b592617f61f8a4
Melissa He <melissahe@ac.c4q.nyc>
2017-10-21 16:31:55 -0400

    Add enums to model

--- ticTacToe/ticTacToeBrain.swift
+++ ticTacToe/ticTacToeBrain.swift
@@ -8,47 +8,60 @@
 
 import Foundation
 
-class ticTacToeBrain {
-    var playerTurn: Int = 1
-    //1 = player1
-    //2 = player2
+class ticTacToeBrain { //the model
+    enum PlayerTurn: Int {
+        case one = 1
+        case two = 2
+        
+        mutating func changeTurn() {
+            (self == .one) ? (self = .two) : (self = .one)
+        }
+    }
     
-//    let winConditions = [
-//                    [1,2,3],
-//                    [4,5,6],
-//                    [7,8,9],
-//                    [1,4,7],
-//                    [2,5,8],
-//                    [3,6,9],
-//                    [1,5,9]
-//                ]
-    
-    var gameMatrix: [[String]] = [
-                                    ["", "", ""],
-                                    ["", "", ""],
-                                    ["", "", ""]
-                                ]
+    enum GameBoard: String {
+        case x = "X"
+        case o = "O"
+        case blank = ""
+    }
     
     var win = false
-    var tie = false
-    
-    enum WinConditions: String {
-        case XXX
-        case OOO
-    }
-    
-    func changePlayerTurn() {
-        (playerTurn == 1) ? (playerTurn = 2) : (playerTurn = 1)
-    }
+    var moveCount = 0
+    var playerTurn: PlayerTurn = .one
+    var player1Score = 0
+    var player2Score = 0 //if reaches 9, tie = true
+    var board: [[GameBoard]] = [[.blank, .blank, .blank],
+                                [.blank, .blank, .blank],
+                                [.blank, .blank, .blank]]
     
     func checkForWin() {
-        for row in 0..<gameMatrix.count-1 {
-            for column in 0..<gameMatrix.count-1 {
-                
+        for number in 0..<board.count {
+            //horizontal win
+            if board[number][0] != .blank && board[number][0] == board[number][1] && board[number][1] == board[number][2] {
+                win = true
+            } else if board[0][number] != .blank && board[0][number] == board[1][number] && board[1][number] == board[2][number] {
+                //vertical win
+                win = true
+            } else if board[0][0] != .blank && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+                //diagonal 1 win
+                win = true
+            } else if board[2][0] != .blank && board[2][0] == board[1][1] && board[1][1] == board[0][2] { //diagonal 2 win
+                win = true
             }
         }
-//        if (playerOneChoices.count + playerTwoChoices.count == 9) &&  {
-//            tie = true
-//        }
+        moveCount += 1
+    }
+    
+    func updatePlayerScores() {
+        if win {
+            (playerTurn == .one) ? (player1Score += 1) : (player2Score += 1)
+        }
+    }
+    
+    func resetGame() {
+        win = false
+        playerTurn = .one
+        board = [[.blank, .blank, .blank],
+                 [.blank, .blank, .blank],
+                 [.blank, .blank, .blank]]
     }
 }

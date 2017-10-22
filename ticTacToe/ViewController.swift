09c5100962da827a3dd6dae4766c9e35e58827d2
Izza Nadeem <izza.nadeem99@gmail.com>
2017-10-21 20:06:01 -0400

    Finished ticTacToe Game

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -9,9 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var noughtScore: UILabel!
+    @IBOutlet weak var crossScore: UILabel!
+    @IBOutlet var allButtons: [UIButton]!
+    @IBOutlet weak var winCount: UILabel!
+    var ticTacToe = ticTacToeBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Execute code here
+        winLabel.text = "Let's go players"
+    }
+    
+    @IBAction func restartButton(_ sender: UIButton) {
+        for button in allButtons {
+            button.isEnabled = true
+        }
+        winLabel.text = "Lets go Players"
+        for buttons in allButtons {
+            buttons.setImage(nil, for: .normal)
+            ticTacToe.resetGameState()
+        }
+    }
+    
+    
+    @IBOutlet weak var winLabel: UILabel!
+    @IBAction func crossButtonPressed(_ sender: UIButton) {
+        ticTacToe.turnCount += 1
+        if ticTacToe.gameState[sender.tag-1] == 0  {
+            ticTacToe.gameState[sender.tag-1] = ticTacToe.activePlayer.rawValue
+            if ticTacToe.activePlayer == .one {
+                sender.setImage(#imageLiteral(resourceName: "Cross"), for: .normal)
+                
+                
+            }
+            else if ticTacToe.activePlayer == .two {
+                sender.setImage(#imageLiteral(resourceName: "Nought"), for: .normal)
+                
+            }
+        }
+        ticTacToe.winningCheck()
+        if ticTacToe.win {
+            //if someone wins
+            //check who won
+            if ticTacToe.activePlayer == .one {
+                crossScore.text = "\(ticTacToe.crossWins)"
+                winLabel.text = "Cross has won"
+            } else {
+                winLabel.text = "Nought has won"
+                noughtScore.text = "\(ticTacToe.noughtWins)"
+            }
+            for button in allButtons {
+                button.isEnabled = false
+            }
+        }
+        else if ticTacToe.turnCount == 9 {
+            winLabel.text = "It a Draw"
+        }
+         ticTacToe.switchPlayer()
     }
 }
+
+
+
+
+
+
+
+
 
--- ticTacToe/ticTacToeBrain.swift
+++ ticTacToe/ticTacToeBrain.swift
@@ -9,5 +9,46 @@
 import Foundation
 
 class ticTacToeBrain {
-
+    enum Players:Int {
+        case one  = 1
+        case two  = 2
+    }
+    
+    var crossWins = 0
+    var noughtWins = 0
+    var win = false
+    var activePlayer: Players = .one
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    var turnCount = 0
+    let winningCombination = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    
+    func resetGameState() {
+        gameState = [0,0,0,0,0,0,0,0,0]
+        turnCount = 0
+        win = false
+        activePlayer = .one
+    }
+    func winningCheck() {
+        for combination in winningCombination {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                if activePlayer == .one {
+                    crossWins += 1
+                    win = true
+                } else if activePlayer == .two  {
+                    noughtWins += 1
+                    win = true
+                }
+            }
+        }
+    }
+    func switchPlayer() {
+        if activePlayer == .one {
+            activePlayer = .two
+        }
+        else if activePlayer == .two {
+            activePlayer = .one
+        }
+    }
 }
+
+

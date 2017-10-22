30263f1cf97ff3469788a3dc287537032a1e01e5
Gloria <gloriaewashington@gmail.com>
2017-10-22 00:05:37 -0400

    initial commit

--- ticTacToe/ticTacToeBrain.swift
+++ ticTacToe/ticTacToeBrain.swift
@@ -9,5 +9,44 @@
 import Foundation
 
 class ticTacToeBrain {
-
+    
+    var counter = 9
+    
+    var playerOne = Set<Int>()
+    var playerTwo = Set<Int>()
+    
+    
+    enum PlayerTurn: Int {
+        case playerOne = 1
+        case playerTwo = 2
+    }
+    
+    var win = false
+    
+    func checkWin() {
+        var winCondition: [Set<Int>] =
+            [ [1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9],
+              [1,4, 7],
+              [2,5,8],
+              [3,6,9],
+              [1,5,9],
+              [3,5,7]
+        ]
+        for array in winCondition {
+            switch counter {
+            case _ where counter % 2 == 1:
+                if array.isSubset(of: playerOne) {
+                    win = true
+                }
+            case _ where counter % 2 == 0:
+                if array.isSubset(of: playerTwo) {
+                    win = true
+                }
+                    default:
+                    break
+                }
+            }
+}
 }

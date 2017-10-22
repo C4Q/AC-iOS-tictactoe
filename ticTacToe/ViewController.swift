af401e2db8eb8ad013a4f28d6071839840baa83e
Nicole Souvenir <github@nicolesouvenir.com>
2017-10-22 18:31:47 -0400

    NS: first commit of tictactoe

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -9,9 +9,108 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var btnOne: GameButton!
+    @IBOutlet weak var btnTwo: GameButton!
+    @IBOutlet weak var btnThree: GameButton!
+    @IBOutlet weak var btnFour: GameButton!
+    @IBOutlet weak var btnFive: GameButton!
+    @IBOutlet weak var btnSix: GameButton!
+    @IBOutlet weak var btnSeven: GameButton!
+    @IBOutlet weak var btnEight: GameButton!
+    @IBOutlet weak var btnNine: GameButton!
+    @IBOutlet weak var newGameBtn: UIButton!
+    
+    @IBOutlet weak var playerOneScore: UILabel!
+    @IBOutlet weak var playerTwoScore: UILabel!
+    @IBOutlet weak var playerTurnLabel: UILabel!
+    @IBOutlet var playerWinLabel: UILabel!
+    
+    @IBOutlet weak var winImage: UIImageView!
+    @IBOutlet weak var drawImage: UIImageView!
+    
+    
+    var buttons: [GameButton] = []
+    var gameRules = ticTacToeBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Execute code here
+        // Do any additional setup after loading the view
+        playerTurnLabel.text = "Player One Turn!"
+        playerWinLabel.isHidden = true
+        winImage.isHidden = true
+        //drawImage.isHidden = true
+        //updateScore()
+        buttons = [btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine]
+    }
+    
+    @IBAction func gameButtonPressed(_ sender: GameButton) {
+        gameRules.turnCounter += 1
+        if gameRules.player {
+            //playerOne
+            gameRules.playerOneArray.insert(sender.tag)
+            sender.setBackgroundImage(#imageLiteral(resourceName: "ximg"), for: .normal)
+            playerTurnLabel.text = "Player Two's Turn!"
+        } else {
+            //playerTwo
+            gameRules.playerTwoArray.insert(sender.tag)
+            sender.setBackgroundImage(#imageLiteral(resourceName: "oimg") , for: .normal)
+            playerTurnLabel.text = "Player One's Turn!"
+        }
+        sender.isEnabled = false
+        
+        //Win condition check
+        gameRules.winningCondition()
+        if gameRules.win == true {
+            playerTurnLabel.isHidden = true
+            if gameRules.player {
+                playerWinLabel.text = "Player One WINS!"
+                playerWinLabel.isHidden = false
+                //sets win UIImage
+                winImage.isHidden = false
+            } else {
+                playerWinLabel.text = "Player Two WINS!"
+                playerWinLabel.isHidden = false
+                winImage.isHidden = false
+            }
+            updateScore()
+        } else if gameRules.win == false && gameRules.turnCounter == 9 {
+         playerTurnLabel.isHidden = true
+            playerWinLabel.text = "It's a DRAW!"
+            playerWinLabel.isHidden = false
+            //sets draw UIImage
+            //drawImage.isHidden = false
+        }
+        gameRules.playerTurn(player: gameRules.player)
+    }
+    
+    func updateScore(){
+        playerOneScore.text = "P1 score: \(gameRules.playerOneScore)"
+        playerTwoScore.text = "P2 score: \(gameRules.playerTwoScore)"
+    }
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        newGame()
+    }
+    
+    //newGame function
+    func newGame() {
+        for button in buttons {
+            button.setBackgroundImage(nil, for: .normal)
+            button.isEnabled = true
+            newGameBtn.isHidden = false
+        }
+        winImage.isHidden = true
+        //drawImage.isHidden = true
+        gameRules.playerReset()
+        gameRules.player = true
+        playerWinLabel.isHidden = true
+        playerTurnLabel.isHidden = false
+        playerTurnLabel.text = "Player One's Turn!"
+        
     }
 }
+
+
+
 
--- ticTacToe/ticTacToeBrain.swift
+++ ticTacToe/ticTacToeBrain.swift
@@ -9,5 +9,58 @@
 import Foundation
 
 class ticTacToeBrain {
-
+    var playerOneArray = Set<Int>()
+    var playerTwoArray = Set<Int>()
+    var win = false
+    var player: Bool = true
+    //counter for draw conditon
+    var turnCounter = 0
+    //will keep winning score for each player
+    var playerOneScore = 0
+    var playerTwoScore = 0
+    
+    
+    //Possible wins
+    let winCombinations: [Set<Int>] =   [[1,2,3],
+                                         [4,5,6],
+                                         [7,8,9],
+                                         [1,4,7],
+                                         [2,5,8],
+                                         [3,6,9],
+                                         [1,5,9],
+                                         [3,5,7]]
+    
+    //Winning conditon function
+    func winningCondition() {
+        for possibleWins in winCombinations {
+            if player {
+                if possibleWins.isSubset(of: playerOneArray){
+                    win = true
+                    playerOneScore += 1
+                }
+            } else {
+                if possibleWins.isSubset(of: playerTwoArray){
+                    win = true
+                    playerTwoScore += 1
+                }
+            }
+        }
+    }
+    
+    //Player reset function
+    func playerReset () {
+        playerOneArray = Set<Int>()
+        playerTwoArray = Set<Int>()
+        turnCounter = 0
+        win = false
+    }
+    
+    //Player turn function
+    func playerTurn(player: Bool) {
+        if player {
+            self.player = false //player two
+        } else {
+            self.player = true //player one
+        }
+    }
 }

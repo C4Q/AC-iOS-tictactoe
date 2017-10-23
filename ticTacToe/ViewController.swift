e64c19787ab7c1066a84ed59f6a63c879b9c4c05
Reiaz Gafar <reiazgafar@ac.c4q.nyc>
2017-10-22 22:14:04 -0400

    Reiaz Gafar tictactoe HW

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -11,7 +11,95 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Execute code here
+        // Clears button titles
+        for i in ticTacToeButtons {
+            i.setTitle(" ", for: .normal)
+        }
+        // Let player one go first
+        turnLabel.text = playerOneTurnText
+        // Hide end game buttons
+        winLabel.isHidden = true
+    }
+    
+    // Create an instance of the model class
+    let ticTacToeModel = ticTacToeBrain()
+    
+    // Outlets
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet var ticTacToeButtons: [GameButton]!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var turnLabel: UILabel!
+    
+    // Create the matrix by calling one from the model
+    lazy var ticTacToeMatrix = ticTacToeModel.startMatrix
+    // Set turn text by using enum from model
+    lazy var playerOneTurnText = ticTacToeModel.turn1.rawValue
+    lazy var playerTwoTurnText = ticTacToeModel.turn2.rawValue
+    
+    @IBAction func ticTacToeButtonPressed(_ sender: GameButton) {
+        // Button action if text is for player 1
+        // Adds "X" to the matrix, sets turn to other player and disables pressed button
+        if turnLabel.text == playerOneTurnText {
+            ticTacToeMatrix[sender.row][sender.column] = "X"
+            sender.setTitle("X", for: .normal)
+            turnLabel.text = playerTwoTurnText
+            sender.isUserInteractionEnabled = false
+        // Button action if text is for player 2
+        // Adds "O" to the matrix, sets turn to other player and disables pressed button
+        } else {
+            ticTacToeMatrix[sender.row][sender.column] = "O"
+            sender.setTitle("O", for: .normal)
+            turnLabel.text = playerOneTurnText
+            sender.isUserInteractionEnabled = false
+        }
+        
+        // Use logic from model to determin win conditions
+        let win = ticTacToeModel.isThereAWinner(ticTacToeMatrix: ticTacToeMatrix)
+        print(win)
+        
+        // Uses win to determine outcome of game and disables all buttons except for play again
+        switch win {
+        case _ where win.0 == "X" :
+            winLabel.isHidden = false
+            winLabel.text = "Player One Wins!"
+            turnLabel.text = "The game has ended."
+            for i in ticTacToeButtons {
+                i.isUserInteractionEnabled = false
+            }
+            playAgainButton.isHidden = false
+        case _ where win.0 == "O" :
+            winLabel.isHidden = false
+            winLabel.text = "Player Two Wins!"
+            turnLabel.text = "The game has ended."
+            for i in ticTacToeButtons {
+                i.isUserInteractionEnabled = false
+            }
+            playAgainButton.isHidden = false
+        case _ where win.0 == "D" :
+            winLabel.isHidden = false
+            winLabel.text = "It's a Draw!"
+            turnLabel.text = "The game has ended."
+            playAgainButton.isHidden = false
+        default:
+            turnLabel.text = turnLabel.text
+            
+        }
+
+    }
+    
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+        // Reset tic tac toe matrix
+        ticTacToeMatrix = ticTacToeModel.startMatrix
+        // Reset all buttons
+        for i in ticTacToeButtons {
+            i.isUserInteractionEnabled = true
+            i.setTitle(" ", for: .normal)
+        }
+        // Sets it to player one's turn
+        turnLabel.text = playerOneTurnText
+        // Hide win label
+        winLabel.isHidden = true
     }
 }
 
--- ticTacToe/ticTacToeBrain.swift
+++ ticTacToe/ticTacToeBrain.swift
@@ -7,7 +7,70 @@
 //
 
 import Foundation
+import UIKit
+
+
+
 
 class ticTacToeBrain {
+    
+    // enum with raw value of string to hold player turn text
+    enum playerTurn: String {
+        case one = "Player One's Turn.  Place an X"
+        case two = "Player Two's Turn.  Place an O"
+    }
+    let turn1 = playerTurn.one
+    let turn2 = playerTurn.two
+    
+    // create the start matrix to begin the game and to reset the game
+    let startMatrix = [["0","1","2"],
+                       ["3","4","5"],
+                       ["6","7","8"]]
+    
+    
+    
+    // Function to check win conditions
+    func isThereAWinner(ticTacToeMatrix: [[String]]) -> (String, Bool) {
+        let flatMatrix = ticTacToeMatrix.flatMap { $0 }
+        // row 1
+        if flatMatrix[0] == flatMatrix[1] && flatMatrix[0] == flatMatrix[2] {
+            return (flatMatrix[0], true)
+        // row 2
+        } else if flatMatrix[3] == flatMatrix[4] && flatMatrix[3] == flatMatrix[5] {
+            return (flatMatrix[3], true)
+        // row 3
+        } else if flatMatrix[6] == flatMatrix[7] && flatMatrix[6] == flatMatrix[8] {
+            return (flatMatrix[6], true)
+        // column 1
+        } else if flatMatrix[0] == flatMatrix[3] && flatMatrix[0] == flatMatrix[6] {
+            return (flatMatrix[0], true)
+        // column 2
+        } else if flatMatrix[1] == flatMatrix[4] && flatMatrix[1] == flatMatrix[7] {
+            return (flatMatrix[1], true)
+        // column 3
+        } else if flatMatrix[2] == flatMatrix[5] && flatMatrix[2] == flatMatrix[8] {
+            return (flatMatrix[2], true)
+        // diagonal
+        } else if flatMatrix[0] == flatMatrix[4] && flatMatrix[0] == flatMatrix[8] {
+            return (flatMatrix[0], true)
+        // diagonal
+        } else if flatMatrix[2] == flatMatrix[4] && flatMatrix[2] == flatMatrix[6] {
+            return (flatMatrix[2], true)
+        // any other combo
+        } else {
+            var allFilledIn = true
+            for i in flatMatrix {
+                if i != "X" && i != "O" {
+                    allFilledIn = false
+                }
+            }
+            if allFilledIn == true {
+                return ("D", false)
+            } else {
+                return (" ", false)
+            }
+        }
+    }
+    
 
 }
